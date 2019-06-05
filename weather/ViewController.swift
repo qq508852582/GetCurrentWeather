38cb0034a55075210979071679a2553523ad84ac
JiangXiaoMing <>
2019-06-05 20:52:56 +0800

    project initialize

---
+++ weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by JiangXiaoMing on 2019/6/5.
+//  Copyright © 2019 JiangXiaoMing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
